18ddc14c39779e723896ac01fa13e428f315a2ed
sehnryr <youn@melois.dev>
2023-07-06 22:17:30 +0200

    feat: :sparkles: create `get_history`
    Implement `get_history` function to get the data for a givent date from the API.

--- relics_run_api/history.py
+++ relics_run_api/history.py
@@ -47,3 +47,75 @@ def get_history_dates() -> List[date]:
 
     # Return the dates
     return dates
+
+
+def get_history(date: date) -> Dict[str, List[Dict[str, Union[str, int, float]]]]:
+    """Get the data for a given date from the API.
+
+    :param date: The date for which to get the data.
+    :type date: date
+    :return: The data for the given date.
+    :rtype: dict
+    :raises requests.HTTPError: If there is an error with the request.
+
+    Usage::
+
+        >>> from datetime import date
+        >>> from relics_run_api import get_history
+        >>> get_history(date=date(2023, 7, 2))
+        {
+            'Secura Dual Cestra': [
+                {
+                    'datetime': '2023-07-02T00:00:00.000+00:00',
+                    'volume': 8,
+                    'min_price': 25,
+                    'max_price': 40,
+                    'open_price': 30,
+                    'closed_price': 34,
+                    'avg_price': 32.5,
+                    'wa_price': 32.375,
+                    'median': 30.0,
+                    'moving_avg': 30.2,
+                    'donch_top': 40,
+                    'donch_bot': 20,
+                    'id': '64a20ff4ca73520023c44a7f'
+                }, {
+                    'datetime': '2023-07-02T00:00:00.000+00:00',
+                    'volume': 435,
+                    'min_price': 10,
+                    'max_price': 15,
+                    'avg_price': 12.5,
+                    'wa_price': 10.062,
+                    'median': 13,
+                    'order_type':
+                    'buy',
+                    'moving_avg': 21.6,
+                    'id': '64a20ff4ca73520023c44a81'
+                }, {
+                    'datetime': '2023-07-02T00:00:00.000+00:00',
+                    'volume': 117,
+                    'min_price': 20,
+                    'max_price': 45,
+                    'avg_price': 32.5,
+                    'wa_price': 32.846,
+                    'median': 30,
+                    'order_type': 'sell',
+                    'moving_avg': 25.3,
+                    'id': '64a20ff4ca73520023c44a80'
+                }
+            ],
+            [...]
+        }
+    """
+
+    # Build the URL
+    url: Final[str] = API_HISTORY_DATE_URL.format(date=date.isoformat())
+
+    # Get the data from the API
+    response: Final[Response] = requests.get(url)
+
+    # Check for errors
+    response.raise_for_status()
+
+    # Return the data
+    return response.json()
