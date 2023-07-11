3492a9bfddadc0dc63c57a48001bbcbf3cebcf16
sehnryr <youn@melois.dev>
2023-07-11 06:35:22 +0200

    feat: :sparkles: create `get_market_data_item_info`
    Implement `get_market_data_item_info` to get the list of item info for which there is data.

--- relics_run_api/__init__.py
+++ relics_run_api/__init__.py
@@ -1,4 +1,8 @@
 from relics_run_api.common import RELIC_RARITY
 from relics_run_api.history import get_history, get_history_dates
 from relics_run_api.index import get_index
-from relics_run_api.market_data import get_market_data_item_ids, get_market_data_items
+from relics_run_api.market_data import (
+    get_market_data_item_ids,
+    get_market_data_item_info,
+    get_market_data_items,
+)
--- relics_run_api/market_data.py
+++ relics_run_api/market_data.py
@@ -5,6 +5,7 @@ from .base import API_BASE_URL
 
 API_MARKET_DATA_URL: Final[str] = API_BASE_URL + "/market_data"
 API_MARKET_DATA_ITEM_IDS_URL: Final[str] = API_MARKET_DATA_URL + "/item_ids.json"
+API_MARKET_DATA_ITEM_INFO_URL: Final[str] = API_MARKET_DATA_URL + "/item_info.json"
 API_MARKET_DATA_ITEMS_URL: Final[str] = API_MARKET_DATA_URL + "/items.json"
 
 
@@ -33,6 +34,42 @@ def get_market_data_item_ids() -> Dict[str, str]:
     response.raise_for_status()
 
     # Return the list of item IDs
+    return response.json()
+
+
+def get_market_data_item_info() -> Dict[str, dict]:
+    """Get the list of item info for which there is data.
+
+    :return: The list of item info for which there is data.
+    :rtype: Dict[str, dict]
+    :raises requests.HTTPError: If there is an error with the request.
+
+    Usage::
+
+        >>> from relics_run_api import get_market_data_item_info
+        >>> get_market_data_item_info()
+        {
+            "54aae292e7798909064f1575": {
+                "set_items": [],
+                "item_id": "54aae292e7798909064f1575",
+                "tags": [
+                    "syndicate",
+                    "weapon",
+                    "secondary"
+                ],
+                "mod_max_rank": null,
+                "subtypes": []
+            },
+            ...
+        }
+    """
+    # Get the list of item info from the API
+    response: Final[Response] = requests.get(API_MARKET_DATA_ITEM_INFO_URL)
+
+    # Check for errors
+    response.raise_for_status()
+
+    # Return the list of item info
     return response.json()
 
 
