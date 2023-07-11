eb40903234da2b24d58de629cc9fc4110c6a9286
sehnryr <youn@melois.dev>
2023-07-11 06:31:23 +0200

    feat: :sparkles: create `get_market_data_item_ids`
    Implement `get_market_data_item_ids` to get the list of item IDs for which there is data.

--- relics_run_api/market_data.py
+++ relics_run_api/market_data.py
@@ -4,7 +4,36 @@ from typing import Dict, List, Final
 from .base import API_BASE_URL
 
 API_MARKET_DATA_URL: Final[str] = API_BASE_URL + "/market_data"
+API_MARKET_DATA_ITEM_IDS_URL: Final[str] = API_MARKET_DATA_URL + "/item_ids.json"
 API_MARKET_DATA_ITEMS_URL: Final[str] = API_MARKET_DATA_URL + "/items.json"
+
+
+def get_market_data_item_ids() -> Dict[str, str]:
+    """Get the list of item IDs for which there is data.
+
+    :return: The list of item IDs for which there is data.
+    :rtype: Dict[str, str]
+    :raises requests.HTTPError: If there is an error with the request.
+
+    Usage::
+
+        >>> from relics_run_api import get_market_data_item_ids
+        >>> get_market_data_item_ids()
+        {
+            "Secura Dual Cestra": "54aae292e7798909064f1575",
+            "Creeping Bullseye": "54ca39abe7798915c1c11e10",
+            "Mutalist Alad V Assassinate (Key)": "54d4c727e77989281cc7d753",
+            ...
+        }
+    """
+    # Get the list of item IDs from the API
+    response: Final[Response] = requests.get(API_MARKET_DATA_ITEM_IDS_URL)
+
+    # Check for errors
+    response.raise_for_status()
+
+    # Return the list of item IDs
+    return response.json()
 
 
 def get_market_data_items() -> List[Dict[str, str]]:
